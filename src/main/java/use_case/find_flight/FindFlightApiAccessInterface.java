274448329221eb5003817c7acfd54196b0ff43c7
Henry1332 <1774004479@qq.com>
2025-12-01 00:25:05 -0500

    Fixed checkstyle issues in FindFlightApiAccessInterface. Closes #50

--- src/main/java/use_case/find_flight/FindFlightApiAccessInterface.java
+++ src/main/java/use_case/find_flight/FindFlightApiAccessInterface.java
@@ -1,9 +1,21 @@
 package use_case.find_flight;
 
-import entity.Flight;
 import java.util.List;
 
+import entity.Flight;
+
 public interface FindFlightApiAccessInterface {
+
+    /**
+     * Searches for flights based on the provided criteria.
+     *
+     * @param originLocationCode      The starting location code.
+     * @param destinationLocationCode The destination location code.
+     * @param departureDate           The date of departure.
+     * @param adults                  Number of adult passengers.
+     * @param nonstop                 Whether the flight should be non-stop.
+     * @return A list of Flight objects matching the criteria.
+     */
     List<Flight> search(String originLocationCode,
                   String destinationLocationCode,
                   String departureDate,
